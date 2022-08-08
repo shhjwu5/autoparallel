96cb125f7b3b4cc6143959cf15e6ed648ecb2034
shhjwu5 <shhjwu5@sjtu.edu.cn>
2022-08-08 19:31:49 +0800

    Modified infrastructure and task Add parallel implementation with pytorch distributed Optimziation remain development

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
-import task
-import profile
+import torch.distributed as dist
 import torch.nn as nn
 import torch
 
-"""
-当前任务：
-1、调研硬件的建模（标准化测试？），进而修改GBDT的代码来做prediction
-2、完善TaskManager，写出layer+batch生成manager.tasklist
-3、完善Infrastructure，用邻接表写拓扑结构（整个写一个infrastructure类？
-4、看一个parallel算法
-5、torch.distributed, 研究能否通信
-Inputs are temporally a Tasklist
-"""
-Layers = [task.LayerPacket(layer=nn.Linear(in_features=3, out_features=2), size=(1, 1, 3)),
-          task.LayerPacket(layer=nn.Linear(in_features=2, out_features=1), size=(1, 1, 2))]
+from task import Task, TaskManager,Task,LayerPacket
+from infrastructure import Infrastructure,Node,Link
 
-Batches = [torch.Tensor([1, 2, 3])]
+dist.init_process_group(backend="nccl")
+rank = dist.get_rank()
+world_size = dist.get_world_size()
 
-Tasklist = {
-    0: task.Task(layer=Layers[0], task_to=1, batch_num=0),
-    1: task.Task(layer=Layers[1], task_from=0, batch_num=0)
-}
-"""
-layerpacket = Tasklist[0].layer
-print(layerpacket)
-print(profile.generate_features(layerpacket))
-print(profile.profile_time_consumption(layerpacket))
-"""
+layers = [nn.Linear(in_features=3, out_features=2).to(torch.device("cuda:0")),
+          nn.Linear(in_features=2, out_features=3).to(torch.device("cuda:1")),
+          nn.Linear(in_features=3, out_features=1).to(torch.device("cuda:2"))]
+
+sizes = [(1,3),(1,2),(1,3),(1,1)]
+
+batches = [torch.Tensor([1, 2, 3]),
+           torch.Tensor([1,2,3])]
+
+task_manager = TaskManager()
+for batch_num,_ in enumerate(batches):
+    for layer_num,layer in enumerate(layers):
+        layer_packet = LayerPacket(layer,sizes[layer_num])
+        task_manager.add_task(Task((batch_num,layer_num),[layer_packet],batch_num))
+
+nodes = [Node(torch.device("cuda:%d"%(i)),100,50) for i in range(3)]
+infrastructure = Infrastructure(nodes)
+for i in range(3):
+    for j in range(i,3):
+        infrastructure.add_link(i,j,Link(0.000009,32))
+
+schedule = [0,1,2]
+for task_id,cur_task in task_manager.tasks.items():
+    cur_task.set_device(device_type="cur",device_num=task_id[1])
+    if task_id[1]-1>=0:
+        cur_task.set_device(device_type="from",device_num=task_id[1]-1)
+    if task_id[1]+1<=2:
+        cur_task.set_device(device_type="to",device_num=task_id[1]+1)
+
+    cur_task.visualize()
+
+for task_id,cur_task in task_manager.tasks.items():
+    if rank==task_id[1]:
+        cur_task.execute(batches[task_id[0]])
 
 
-Tasklist[0].get_data_from_batch(Batches)
-Tasklist[0].pass_result(Tasklist)
-Tasklist[1].pass_result(Tasklist)
